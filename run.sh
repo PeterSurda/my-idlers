0dd3793411c47b11d53d173c799d63a22680cab9
Peter Surda <peter@bitmessage.at>
2024-06-21 14:10:43 +0800

    run.sh for automatic deployments

--- run.sh
+++ run.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
-# Run setup only if .env file doesn't exist.
-if [ ! -e .env.production ]
-then
-cat > .env.production << EOF
-APP_NAME=MyIdlers
-APP_DEBUG=false
-APP_KEY=
+# Load environment variables from .env.production
+set -o allexport
+source .env.production
+set +o allexport
 
-DB_CONNECTION=mysql
-DB_HOST=${DB_HOST}
-DB_DATABASE=${DB_DATABASE}
-DB_USERNAME=${DB_USERNAME}
-DB_PASSWORD=${DB_PASSWORD}
-APP_URL=${APP_URL}
-EOF
-php artisan key:generate --no-interaction --force
+# Check if the database file exists and is not empty
+if [ ! -s $DB_DATABASE ]
+then
+    touch $DB_DATABASE
+    php artisan migrate:fresh --seed --force
 fi
 
-# php artisan migrate:fresh --seed
 php artisan serve --host=0.0.0.0 --port=8000 --env=production
